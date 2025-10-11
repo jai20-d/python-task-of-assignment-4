90e05664af6fa8392651e3b2c01752e888479df5
jai <jaitomarjai20@gmail.com>
2025-10-11 15:42:47 +0530

    Update assignment 4 task 2.py

--- ERROR AND FILES/assignment 4 task 2.py
+++ ERROR AND FILES/assignment 4 task 2.py
@@ -1,4 +1,12 @@
-file= open('output','a')
-writing_file= file.write(' \nlearning file handeling in python.')
-print(writing_file)
+file= open('output.txt','w')
+write=file.write('hello, Python!')
+file.close()
+
+file= open('output.txt','a')
+writing_file= file.write(' \nlearning file handling in python.Data succesfully appended.')
+file.close()
+
+file= open('output.txt','r')
+reading_file= file.read()
+print(reading_file)
 file.close()
